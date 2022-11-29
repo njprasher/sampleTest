0bbfd1cb4ee5183ed2bbff68534a9ba818a8abd4
njprasher <prasher.neeraj99@gmail.com>
2022-11-28 22:47:45 -0500

    Highest and Second Highest in an Array

---
+++ src/main/java/org/example/SecondHighestNumberInArray.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class SecondHighestNumberInArray {
+    public static void main(String[] args)
+    {
+        int[] arr = { 100,14, 46, 47, 94, 94, 52, 86, 36, 94, 89 };
+        int largest = 0;
+        int secondLargest = 0;
+        System.out.println("The given array is:");
+        for (int j : arr) {
+            System.out.print(j + "\t");
+        }
+        for (int j : arr) {
+            if (j > largest) {
+                secondLargest = largest;
+                largest = j;
+            } else if (j > secondLargest) {
+                secondLargest = j;
+            }
+        }
+        System.out.println("\nSecond largest number is:" + secondLargest);
+        System.out.println("Largest Number is: "  +largest);
+    }
+}
