d466f0afd2e2bc03e5f3b3e72edad23e0aaa9a34
njprasher <prasher.neeraj99@gmail.com>
2022-11-28 22:36:00 -0500

    Count occurrence of Character in a String

---
+++ src/main/java/org/example/CountOccurrenceOfChar.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class CountOccurrenceOfChar{
+    public static void main(String[] args)
+    {
+        String str = "ABCDABCDABC";
+        //HashMap char as a key and occurrence as a value
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        for (int i = str.length() - 1; i >= 0; i--)
+        {
+            if(charCount.containsKey(str.charAt(i)))
+            {
+                int count = charCount.get(str.charAt(i));
+                charCount.put(str.charAt(i), ++count);
+            }
+            else
+            {
+                charCount.put(str.charAt(i),1);
+            }
+        }
+        System.out.println(charCount);
+    }
+}
